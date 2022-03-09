c8051e9757fd80ef0ce75d591b28c6a1e3908589
Aslan <tavorktar@gmail.com>
2022-03-10 00:31:06 +0600

    add local storage

--- script/generateGoodsPage.js
+++ script/generateGoodsPage.js
@@ -1,6 +1,9 @@
 import { getData } from "./getData.js"
+import userData from "./userData.js";
 
-const wishList = ['idd001', 'idd034' ,'idd023' ,'idd099'];
+const COUNTER = 6;
+
+
 
 const generateGoodsPage = () => {
 
@@ -9,27 +12,48 @@ const generateGoodsPage = () => {
 
     const generateCards = (data) => {
         goodsList.textContent = '';
+        if (!data.length) {
+            const goods = document.querySelector('.goods')
+            goods.textContent = location.search === '?wishlist' ? 
+            'Список желаний пуст' : 
+            'К сожалению по вашему запросу ничего не найдено'
+            return
+        }
         
         data.forEach(item => {
+
+            const {name, count, price, description, img, id} = item
+
             goodsList.insertAdjacentHTML('afterbegin', `
             <li class="goods-list__item">
-                <a class="goods-item__link" href="card.html#idd001">
+                <a class="goods-item__link" href="card.html#${id}">
                     <article class="goods-item">
                         <div class="goods-item__img">
-                            <img src=${item.img}>
+                            <img src=${img[0]}
+                            ${img[1] ? `data-second-image=${img[1]}`: ''}>
                         </div>
-                        <p class="goods-item__new">Новинка</p>
-                        <h3 class="goods-item__header">${item.name}</h3>
-                        <p class="goods-item__description">${item.description}</p>
+                        ${count > COUNTER ? `<p class="goods-item__new">Новинка</p>` : ''}
+                        ${!count  ? `<p class="goods-item__new">Нет в наличии</p>` : ''}
+                        <h3 class="goods-item__header">${name}</h3>
+                        <p class="goods-item__description">${description}</p>
                         <p class="goods-item__price">
-                            <span class="goods-item__price-value">${item.price}</span>
+                            <span class="goods-item__price-value">${price}</span>
                             <span class="goods-item__currency"> ₽</span>
                         </p>
-                        <button class="btn btn-add-card" aria-label="Добравить в корзину" data-idd="idd001"></button>
+                        ${count ? `<button class="btn btn-add-card" aria-label="Добравить в корзину" data-idd="${id}"></button>` : ''
+                    }
                     </article>
                 </a>
             </li>
             `)
+        })
+        goodsList.addEventListener('click', (e) => {
+            
+            const btnAddCard = e.target.closest('.btn-add-card') 
+            if(btnAddCard) {
+                e.preventDefault();
+                userData.cartList = btnAddCard.dataset.idd
+            }
         })
     }
 
@@ -42,7 +66,7 @@ const generateGoodsPage = () => {
             getData.search(value, generateCards)
             mainHeader.textContent = `Поиск: ${value}`
         } else if (prop === 'wishlist'){
-            getData.wishList(wishList, generateCards)
+            getData.wishList(userData.wishList, generateCards)
             mainHeader.textContent = `Список желаний`
         } else if(prop === 'cat' || prop === 'subcat') {
             getData.category(prop, value, generateCards)
---
+++ script/generateItemPage.js
@@ -0,0 +1,68 @@
+import { getData } from "./getData.js";
+import userData from "./userData.js";
+
+const NEW_COUNT_ITEM = 6;
+
+const generateItemPage = () => {
+
+    const renderCard = ({category, count, description, id, img, name:itemName, price, subcategory}) => {
+
+        const goodImages = document.querySelector('.good-images')
+        const goodItemNew = document.querySelector('.good-item__new')
+        const goodItemHeader = document.querySelector('.good-item__header')
+        const goodItemDescription = document.querySelector('.good-item__description')
+        const goodItemEmpty = document.querySelector('.good-item__empty')
+        const goodItemPriceValue = document.querySelector('.good-item__price-value')
+        const btnGood = document.querySelector('.btn-good')
+        const btnAddWishlist = document.querySelector('.btn-add-wishlist')
+
+        goodImages.textContent = ''
+        goodItemNew
+        goodItemHeader.textContent = itemName
+        goodItemDescription
+        goodItemEmpty
+        goodItemPriceValue.textContent = price
+        btnGood.dataset.idd = id
+        btnAddWishlist.dataset.idd = id
+
+        img.forEach(item => {
+            goodImages.insertAdjacentHTML('afterbegin', `
+            <div class="good-image__item">
+                <img src="${item}" alt="${itemName} - ${description}">
+            </div>
+            `)
+        });
+
+        if(count >= NEW_COUNT_ITEM) {
+            goodItemNew.style.display = 'block'
+        } else if (!count) {
+            goodItemEmpty.style.display = 'block'
+            btnGood.style.display = 'none'
+        }
+
+        const checkWishList = () => {
+            if (userData.wishList.includes(id)) {
+                btnAddWishlist.classList.add('contains-wishlist')
+            } else {
+                btnAddWishlist.classList.remove('contains-wishlist')
+            }
+        }
+        btnAddWishlist.addEventListener('click', () => {
+            userData.wishList = id;
+            checkWishList()
+        })
+
+        btnGood.addEventListener('click', () => {
+            userData.cartList = id
+        })
+
+        checkWishList()
+
+    }
+
+    if(location.hash && location.pathname.includes('card')){
+        getData.item(location.hash.substring(1), renderCard)
+    }
+
+}
+export default generateItemPage;
